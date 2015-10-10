40064a007f00dc95b2445582697b427620509ce1
wiz <wiz@ubuntu-dev01.yggdrasil.com>
2015-10-10 12:42:18 +0700

    Initial version.

---
+++ messenger/src/main/java/com/yggdrasil/test/messenger/resource/MessageResource.java
@@ -0,0 +1,26 @@
+package com.yggdrasil.test.messenger.resource;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.yggdrasil.test.messenger.model.Message;
+import com.yggdrasil.test.messenger.service.MessageService;
+
+@Path("/messages")
+public class MessageResource {
+	
+	MessageService messageService = new MessageService();
+	
+	@GET
+	//@Produces(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Message> getMessages() {
+		
+		return messageService.getAllMessages();
+	}
+
+}
