40ad804ab38e7d55e301ee91a1a329270fb27fed
wiz <wiz@ubuntu-dev01.yggdrasil.com>
2015-10-11 11:52:33 +0700

    - Added InjectDemoResource, for extracting paramenters and context from request. 3 Techniques (1) Annotations, (2) @Context, (3) @BeanParam.

--- messenger/src/main/java/com/yggdrasil/test/messenger/resource/MessageResource.java
+++ messenger/src/main/java/com/yggdrasil/test/messenger/resource/MessageResource.java
@@ -2,6 +2,7 @@ package com.yggdrasil.test.messenger.resource;
 
 import java.util.List;
 
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -10,9 +11,11 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.yggdrasil.test.messenger.model.Message;
+import com.yggdrasil.test.messenger.resource.beans.MessageFilterBean;
 import com.yggdrasil.test.messenger.service.MessageService;
 
 @Path("/messages")
@@ -22,12 +25,28 @@ public class MessageResource {
 	
 	MessageService messageService = new MessageService();
 	
-	@GET
+//	@GET
 	//@Produces(MediaType.TEXT_PLAIN)
 	//@Produces(MediaType.APPLICATION_XML)
 	//@Produces(MediaType.APPLICATION_JSON)
-	public List<Message> getMessages() {
+//	public List<Message> getMessages() {
+//		
+//		return messageService.getAllMessages();
+//	}
+	
+	@GET
+//	public List<Message> getMessages(@QueryParam("year") int year,
+//									@QueryParam("start") int start,
+//									@QueryParam("size") int size) {
+	public List<Message> getMessages(@BeanParam MessageFilterBean filterBean) {
+		if(filterBean.getYear() > 0) {
+			return messageService.getAllMessagesForYear(filterBean.getYear());
+		}
 		
+		if(filterBean.getStart() >= 0 & filterBean.getSize() >= 0) {
+			return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
+		}
+			
 		return messageService.getAllMessages();
 	}
 	
